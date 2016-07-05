3608e00fc614e505ad5cc3558065259673d37a9f
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-07-05 21:38:51 +0800

    switch from 4 spaces to 2 spaces

--- src/Yalesov/Yaml/Yaml.php
+++ src/Yalesov/Yaml/Yaml.php
@@ -6,28 +6,28 @@ use Symfony\Component\Yaml\Yaml as SymfonyYaml;
 
 class Yaml
 {
-    /**
-     * Yaml parser
-     *
-     * __DIR__ support: (only available for Yaml files, of course!)
-     * - '__DIR__' will behave as expected
-     * - '___DIR___' (an extra underscore around) will be translated as a literal '__DIR__'
-     *
-     * @param  mixed $yaml
-     * @return self
-     */
-    public static function parse($yaml)
-    {
-        ArgValidator::assert($yaml, 'string');
-        if (is_file($yaml) && is_readable($yaml)) {
-            $dir = realpath(dirname($yaml));
-            $yaml = file_get_contents($yaml);
-            $yaml = strtr($yaml, array(
-                '___DIR___' => '__DIR__',
-                '__DIR__' => $dir,
-            ));
-        }
-
-        return SymfonyYaml::parse($yaml);
+  /**
+   * Yaml parser
+   *
+   * __DIR__ support: (only available for Yaml files, of course!)
+   * - '__DIR__' will behave as expected
+   * - '___DIR___' (an extra underscore around) will be translated as a literal '__DIR__'
+   *
+   * @param  mixed $yaml
+   * @return self
+   */
+  public static function parse($yaml)
+  {
+    ArgValidator::assert($yaml, 'string');
+    if (is_file($yaml) && is_readable($yaml)) {
+      $dir = realpath(dirname($yaml));
+      $yaml = file_get_contents($yaml);
+      $yaml = strtr($yaml, array(
+        '___DIR___' => '__DIR__',
+        '__DIR__' => $dir,
+      ));
     }
+
+    return SymfonyYaml::parse($yaml);
+  }
 }
--- test/Yalesov/Test/Yaml/YamlTest.php
+++ test/Yalesov/Test/Yaml/YamlTest.php
@@ -5,68 +5,68 @@ use Yalesov\Yaml\Yaml;
 
 class YamlTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
-        $this->tmpFile = '.yaml';
-        $this->tmpPath = realpath(dirname($this->tmpFile));
-    }
+  public function setUp()
+  {
+    $this->tmpFile = '.yaml';
+    $this->tmpPath = realpath(dirname($this->tmpFile));
+  }
 
-    public function tearDown()
-    {
-        unlink($this->tmpFile);
-    }
+  public function tearDown()
+  {
+    unlink($this->tmpFile);
+  }
 
-    public function testParse()
-    {
-        // yaml stream
-        $yaml = 'foo: bar';
-        $this->assertSame(
-            array('foo' => 'bar'),
-            Yaml::parse($yaml));
+  public function testParse()
+  {
+    // yaml stream
+    $yaml = 'foo: bar';
+    $this->assertSame(
+      array('foo' => 'bar'),
+      Yaml::parse($yaml));
 
-        // yaml file
-        file_put_contents($this->tmpFile, 'foo: bar');
-        $this->assertSame(
-            array('foo' => 'bar'),
-            Yaml::parse($this->tmpFile));
+    // yaml file
+    file_put_contents($this->tmpFile, 'foo: bar');
+    $this->assertSame(
+      array('foo' => 'bar'),
+      Yaml::parse($this->tmpFile));
 
-        // __DIR__
-        file_put_contents($this->tmpFile, 'foo: __DIR__/bar');
-        $this->assertSame(
-            array('foo' => "{$this->tmpPath}/bar"),
-            Yaml::parse($this->tmpFile));
+    // __DIR__
+    file_put_contents($this->tmpFile, 'foo: __DIR__/bar');
+    $this->assertSame(
+      array('foo' => "{$this->tmpPath}/bar"),
+      Yaml::parse($this->tmpFile));
 
-        // ___DIR___
-        file_put_contents($this->tmpFile, 'foo: ___DIR___/bar');
-        $this->assertSame(
-            array('foo' => '__DIR__/bar'),
-            Yaml::parse($this->tmpFile));
+    // ___DIR___
+    file_put_contents($this->tmpFile, 'foo: ___DIR___/bar');
+    $this->assertSame(
+      array('foo' => '__DIR__/bar'),
+      Yaml::parse($this->tmpFile));
 
-        // multiple __DIR__
-        file_put_contents($this->tmpFile, '__DIR__/foo: __DIR__/bar');
-        $this->assertSame(
-            array("{$this->tmpPath}/foo" => "{$this->tmpPath}/bar"),
-            Yaml::parse($this->tmpFile));
+    // multiple __DIR__
+    file_put_contents($this->tmpFile, '__DIR__/foo: __DIR__/bar');
+    $this->assertSame(
+      array("{$this->tmpPath}/foo" => "{$this->tmpPath}/bar"),
+      Yaml::parse($this->tmpFile));
 
-        // multiple ___DIR___
-        file_put_contents($this->tmpFile, '___DIR___/foo: ___DIR___/bar');
-        $this->assertSame(
-            array('__DIR__/foo' => '__DIR__/bar'),
-            Yaml::parse($this->tmpFile));
+    // multiple ___DIR___
+    file_put_contents($this->tmpFile, '___DIR___/foo: ___DIR___/bar');
+    $this->assertSame(
+      array('__DIR__/foo' => '__DIR__/bar'),
+      Yaml::parse($this->tmpFile));
 
-        // altogether
-        file_put_contents($this->tmpFile, "__DIR__/foo: ___DIR___/bar\n___DIR___/bar: __DIR__/foo");
-        $this->assertSame(
-            array(
-                "{$this->tmpPath}/foo" => '__DIR__/bar',
-                '__DIR__/bar' => "{$this->tmpPath}/foo",
-            ),
-            Yaml::parse($this->tmpFile));
+    // altogether
+    file_put_contents($this->tmpFile, "__DIR__/foo: ___DIR___/bar\n___DIR___/bar: __DIR__/foo");
+    $this->assertSame(
+      array(
+        "{$this->tmpPath}/foo" => '__DIR__/bar',
+        '__DIR__/bar' => "{$this->tmpPath}/foo",
+      ),
+      Yaml::parse($this->tmpFile));
 
-        // neither available when passed in a string
-        $yaml = '__DIR__/foo: ___DIR___/bar';
-        $this->assertSame(
-            array('__DIR__/foo' => '___DIR___/bar'),
-            Yaml::parse($yaml));
-    }
+    // neither available when passed in a string
+    $yaml = '__DIR__/foo: ___DIR___/bar';
+    $this->assertSame(
+      array('__DIR__/foo' => '___DIR___/bar'),
+      Yaml::parse($yaml));
+  }
 }
